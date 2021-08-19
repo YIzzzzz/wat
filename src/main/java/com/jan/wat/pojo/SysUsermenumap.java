ff44b10091ef5cf4740a7e5bce8286aa5f0b310a
January_Zeng <15841331413@163.com>
2021-08-19 14:15:08 +0800

    sys_usermenumap

--- src/main/java/com/jan/wat/pojo/SysUsermenumap.java
+++ src/main/java/com/jan/wat/pojo/SysUsermenumap.java
@@ -1,33 +1,32 @@
 package com.jan.wat.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
- * <p>
- * 
- * </p>
- *
- * @author January
- * @since 2021-07-08
+ * @hor:ZXJ
+ * @time:2021/8/19下午1:55
+ * @description
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class SysUserrolemap implements Serializable {
+public class SysUsermenumap {
 
     private static final long serialVersionUID = 1L;
 
     @TableId(value = "ID", type = IdType.AUTO)
     private Integer id;
 
-    @TableField("UserCode")
-    private String usercode;
-
     @TableField("RoleCode")
     private String rolecode;
+
+    @TableField("MenuCode")
+    private String menucode;
+
+    @TableId("UserCode")
+    private String usercode;
 
 }
---
+++ src/main/resources/database/sys_usermenumap.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `sys_usermenumap` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `RoleCode` varchar(100) DEFAULT NULL,
+  `MenuCode` varchar(100) DEFAULT NULL,
+  `UserCode` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `MenuCode` (`MenuCode`),
+  KEY `RoleCode` (`RoleCode`),
+  KEY `FK_sys_usermenumap_sys_user` (`UserCode`),
+  CONSTRAINT `FK__SysRoleFu__Funct__0D7A02861` FOREIGN KEY (`MenuCode`) REFERENCES `sys_menu` (`MenuCode`),
+  CONSTRAINT `FK__SysRoleFu__RoleC__0C85DE4D1` FOREIGN KEY (`RoleCode`) REFERENCES `sys_role` (`RoleCode`),
+  CONSTRAINT `FK_sys_usermenumap_sys_user` FOREIGN KEY (`UserCode`) REFERENCES `sys_user` (`UserCode`)
+) ENGINE=InnoDB AUTO_INCREMENT=13693 DEFAULT CHARSET=utf8mb3CREATE TABLE `sys_usermenumap` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `RoleCode` varchar(100) DEFAULT NULL,
+  `MenuCode` varchar(100) DEFAULT NULL,
+  `UserCode` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `MenuCode` (`MenuCode`),
+  KEY `RoleCode` (`RoleCode`),
+  KEY `FK_sys_usermenumap_sys_user` (`UserCode`),
+  CONSTRAINT `FK__SysRoleFu__Funct__0D7A02861` FOREIGN KEY (`MenuCode`) REFERENCES `sys_menu` (`MenuCode`),
+  CONSTRAINT `FK__SysRoleFu__RoleC__0C85DE4D1` FOREIGN KEY (`RoleCode`) REFERENCES `sys_role` (`RoleCode`),
+  CONSTRAINT `FK_sys_usermenumap_sys_user` FOREIGN KEY (`UserCode`) REFERENCES `sys_user` (`UserCode`)
+) ENGINE=InnoDB AUTO_INCREMENT=13693 DEFAULT CHARSET=utf8mb3
